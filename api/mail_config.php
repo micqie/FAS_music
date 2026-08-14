9b62fc0f8ef44118ab921fa64c6ea104d11dde42
micqie <midu.lago.coc@phinmaed.com>
2026-08-14 21:09:46 +0800

    removed private codess

--- api/mail_config.php
+++ api/mail_config.php
@@ -3,20 +3,10 @@ return [
     'MAIL_HOST' => 'smtp.gmail.com',
     'MAIL_PORT' => 587,
     'MAIL_ENCRYPTION' => 'tls',
-    'MAIL_USERNAME' => 'seso.pabilona.coc@phinmaed.com',
-    'MAIL_PASSWORD' => 'ggpmbclacaijznva',
-    'MAIL_FROM_ADDRESS' => 'seso.pabilona.coc@phinmaed.com',
+    'MAIL_USERNAME' => '',
+    'MAIL_PASSWORD' => '',
+    'MAIL_FROM_ADDRESS' => '',
     'MAIL_FROM_NAME' => 'Father & Sons Music School',
-    'MAIL_REPLY_TO' => 'seso.pabilona.coc@phinmaed.com',
+    'MAIL_REPLY_TO' => '',
     'MAIL_VERIFY_PEER' => false,
 ];
-
-
-
-
-
-
-
-
-
-
